8c672d43985cd77724143b5309fcb31e44d4f192
Inga <inga.s6699@gmail.com>
2021-07-06 16:53:46 +0200

    feature: random color with name in script.js

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 console.log("Welcome to Coulette!");
 
+//generate random new color for the header
 function generateNewHeaderColor() {
-  let color = "salmon";
-  let header = document.querySelector("header");
-  let bgColor = header.style.backgroundColor;
+  const color = randomHexColor();
 
-  if (bgColor === color) {
-    bgColor = "transparent";
-  } else {
-    bgColor = color;
-  }
+  // set hex Name as text of the paragraph
+  const hexText = document.querySelector("#hexColorName");
+  hexText.textContent = color;
 
-  header.style.backgroundColor = bgColor;
+  const header = document.querySelector("header");
+  header.style.backgroundColor = color;
 }
 
-let button = document.querySelector("#genColor");
-button.addEventListener("click", generateNewHeaderColor);
+// search/find 'generate color' button
+let generateColorButton = document.querySelector("#genColor");
+generateColorButton.addEventListener("click", generateNewHeaderColor);
+
+// :::::::: provided code ::::::::
+
+/**
+ * Toggle color of header
+ * Generate random number between min and max
+ */
+function randomNumber(min, max) {
+  const num = Math.random() * (max - min + 1) + min;
+  return Math.floor(num);
+}
+
+/**
+ * Generate random hex number for color
+ */
+function randomHexNumber() {
+  let hex = randomNumber(0, 255).toString(16);
+  if (hex.length === 1) {
+    hex = "0" + hex;
+  }
+  return hex;
+}
+
+/**
+ * Generate random hex color
+ */
+function randomHexColor() {
+  const red = randomHexNumber();
+  const green = randomHexNumber();
+  const blue = randomHexNumber();
+
+  return ("#" + red + green + blue).toUpperCase();
+}
 
 /*function saveNewListItems() {}
 
