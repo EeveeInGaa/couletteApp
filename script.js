4adcbee3327ccb8770734bd198e0cbec3f58acbd
Inga <inga.s6699@gmail.com>
2021-07-08 11:44:34 +0200

    tabula rasa

--- script.js
+++ script.js
@@ -1,43 +1,6 @@
 console.log("Welcome to Coulette!");
 
-let globalColor = "null";
-let color = randomHexColor();
-
-//generate random new color for the header
-function generateNewHeaderColor() {
-  // set hex Name as text of the paragraph
-  const hexText = document.querySelector("#hexColorName");
-  hexText.textContent = color;
-
-  const header = document.querySelector("header");
-  header.style.backgroundColor = color;
-}
-
-//initially chance header color
-generateNewHeaderColor();
-
-// search/find 'generate color' button
-let generateColorButton = document.querySelector("#genColor");
-generateColorButton.addEventListener("click", generateNewHeaderColor);
-
-//search/find 'save color' button
-const saveColorButton = document.querySelector("#saveColor");
-saveColorButton.addEventListener("click", saveNewHeaderColor);
-
-function saveNewHeaderColor() {
-  //Zugriff auf Liste der Farben
-  const colorList = document.querySelector("#colors");
-
-  //element erzeugen und text einfügen
-  const newColorInList = document.createElement("li");
-  const textInList = document.createTextNode(color);
-  newColorInList.appendChild(textInList);
-
-  //setze hintergrundfarbe in neue liste und füge es in listenelemnt als kind ein
-  newColorInList.style.backgroundColor = generateNewHeaderColor;
-
-  colorList.appendChild(newColorInList);
-}
+//tabula rasa
 
 // :::::::: provided code ::::::::
 
